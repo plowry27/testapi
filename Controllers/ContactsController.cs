2520b0143c4ada4fe1b027dd3d8c5ca85d32b68e
Peter Lowry <plowry@gmail.com>
2018-07-04 12:09:01 -0700

    Updated firstname data

--- Controllers/ContactsController.cs
+++ Controllers/ContactsController.cs
@@ -16,7 +16,7 @@ namespace TodoApi.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            Contact con = new Contact() {firstname = "Elizabeth", lastname="Lowry"};
+            Contact con = new Contact() {firstname = "Jennie", lastname="Lowry"};
             var info = con.GetContactInfo();
             return new string[] { info.firstname, info.lastname };
         }
