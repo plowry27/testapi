4b2bbb33f6fbf0bf4cd826d11ce1c3bead3de7e6
Peter Lowry <plowry@gmail.com>
2018-07-04 13:41:19 -0700

    Updated Values Get

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -15,7 +15,7 @@ namespace TodoApi.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "valueA", "valueB" };
         }
 
         // GET api/values/5
