29b09ba586c7c56ecde64eca55f125ad80521b04
Peter Lowry <plowry@gmail.com>
2018-08-16 00:26:07 -0700

    Update comments

--- Entities/csString.cs
+++ Entities/csString.cs
@@ -1,3 +1,4 @@
 //Server=tcp:pcldb.database.windows.net,1433;Initial Catalog=pcldb;Persist Security Info=False;User ID={your_username};Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;
 //oauth: df03c8080e92160bca7c0f9f61504b8ff526f35d73f7323fd2deba6e1920f980
 //App Key: 6cd227fe912f2afbf5560cd211058d84
+//27 and 27
